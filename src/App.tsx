31a99ebb641e703312869c30a95f2302e819f2d4
Oleksandr Holub <oleksandr.h@namecheap.com>
2023-07-25 18:23:17 -0500

    fix: move navigation bar to the center

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Col, Container, Row} from 'react-bootstrap';
 import NavigationBar from './components/NabigationBar/NavigationBar';
 import Header from './components/Header/Header';
-import SocialMediaBar from './components/SocialMediaBar/SocialMediaBar';
 import {AppContextProvider} from './context/AppContext';
 import {data} from './data';
 import Content from './components/Content/Content';
@@ -12,23 +11,14 @@ const App = () => {
     <AppContextProvider value={{...data}}>
       <Container className="desktop-bottom-margin">
         <div className="mt-md-5 mt-sm-2 py-5 d-flex flex-column">
-          <Row className="mb-5 justify-content-end">
-            <Col md="10">
-              <Header />
-            </Col>
-          </Row>
-          <Row className="mb-5 justify-content-end">
-            <Col md="10">
-              <SocialMediaBar />
-            </Col>
-          </Row>
           <Row>
             <Col md="2">
-              <div className="position-fixed">
-                <NavigationBar />
-              </div>
+              <NavigationBar />
             </Col>
-            <Col>
+            <Col md="10">
+              <div className="mb-5">
+                <Header />
+              </div>
               <Content />
             </Col>
           </Row>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,14 @@
 ﻿import React from 'react';
 import pdf from './resume.pdf';
 import ArrowLink from '../ArrowLink';
+import SocialMediaBar from '../SocialMediaBar/SocialMediaBar';
 
 const Header: React.FC = () => (
-  <div className="header mb-md-5">
+  <div className="header">
     <div className="header__title">Oleksandr Holub</div>
     <div className="header__description">Software Engineer</div>
     <ArrowLink className="mt-2" href={pdf} text="View my resume" />
+    <SocialMediaBar />
   </div>
 );
 
